7e3c4ced7075bcf72dcf44b4bea349206ce4a095
danielowie319 <danielowie@gmail.com>
2025-06-30 16:45:03 -0500

    Day1

---
+++ Week3/src/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Helloworld from './components/Helloworld';
+
+
+function App() {
+  return (
+    <div>
+      <h1><Helloworld /></h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Week3/src/my-react-app/src/components/Helloworld.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Helloworld = () => {
+  return (
+    <div>Hello World</div>
+  )
+}
+
+
+export default Helloworld
